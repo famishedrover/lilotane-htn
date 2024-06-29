d8defd6e92b514f9df30e353969b5170f20cc550
Mudit Verma <mudit.verma2014@gmail.com>
2024-06-29 01:17:18 -0700

    push rest of things

---
+++ lib/glucose4/glucose-4/simp/depend.mk
@@ -0,0 +1,42 @@
+/home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/Main.o /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/Main.or /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/Main.od /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/Main.op: \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/Main.cc \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../utils/System.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/IntTypes.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../utils/ParseUtils.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../utils/Options.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Vec.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/XAlloc.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/Dimacs.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/SolverTypes.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Alg.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Map.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Alloc.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../simp/SimpSolver.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Queue.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/Solver.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Heap.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/BoundedQueue.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/Constants.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Clone.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/SolverStats.h
+/home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/SimpSolver.o /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/SimpSolver.or /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/SimpSolver.od /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/SimpSolver.op: \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/SimpSolver.cc \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Sort.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Vec.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/IntTypes.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/XAlloc.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../simp/SimpSolver.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Queue.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/Solver.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Heap.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Alg.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../utils/Options.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../utils/ParseUtils.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/SolverTypes.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Map.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Alloc.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/BoundedQueue.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/Constants.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../mtl/Clone.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../core/SolverStats.h \
+ /home/local/ASURITE/mverma13/work/htnplanner/lilotane/lib/glucose4/glucose-4/simp/../utils/System.h
